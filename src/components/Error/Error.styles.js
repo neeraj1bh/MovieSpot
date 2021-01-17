67717654404624142bf8faefdc5a7200e8e4ea45
neeraj1bh <neeraj1bh@gmail.com>
2021-01-17 10:10:33 +0530

    Added and centered Error message

--- src/components/Error/Error.styles.js
+++ src/components/Error/Error.styles.js
@@ -4,9 +4,13 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   height: 60vh;
+  flex-direction: column;
   align-items: center;
   padding: 20px;
   margin: 20px auto;
+  h3 {
+    text-align: center;
+  }
 `;
 
 export const Content = styled.div`
--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -4,6 +4,7 @@ import { Wrapper, Content } from "./Error.styles";
 const Error = () => (
   <Wrapper>
     <Content>Sorry No Results Found...</Content>
+    <h3>[No Internet Connection or Else the Movie Doesn't Exist]</h3>
   </Wrapper>
 );
 
