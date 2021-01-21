a6f2c3c9cef1630e2037f5b325821e491717e3ed
neeraj1bh <neeraj1bh@gmail.com>
2021-01-21 15:43:44 +0530

    Implemented Routing and added Global Context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import { GlobalStyle } from "./GlobalStyle";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { AuthProvider } from "./contexts/GlobalContext";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Movie from "./components/Movie";
 import Error from "./components/Error";
 import Similar from "./components/Similar";
+import Signup from "./components/Signup/Signup";
+import Login from "./components/Login/Login";
+import Watchlist from "./components/Watchlist";
 
 const App = () => (
   <Router>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/:movieId" component={Movie} />
-      <Route path="/:movieId/similar/" component={Similar} />
-      <Route path="/*" component={Error} />
-    </Switch>
+    <AuthProvider>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+        <Route path="/watchlist" component={Watchlist} />
+        <Route exact path="/:movieId" component={Movie} />
+        <Route path="/:movieId/similar/" component={Similar} />
+        <Route path="/*" component={Error} />
+      </Switch>
 
-    <GlobalStyle />
+      <GlobalStyle />
+    </AuthProvider>
   </Router>
 );
 
