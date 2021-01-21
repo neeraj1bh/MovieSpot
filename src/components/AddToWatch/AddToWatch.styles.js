30620ff5599ade6a8ba6458da765541a94bdb1c9
neeraj1bh <neeraj1bh@gmail.com>
2021-01-21 15:02:32 +0530

    Added new Watchlist component

---
+++ src/components/AddToWatch/AddToWatch.styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 60vh;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  margin: 20px auto;
+  h3 {
+    text-align: center;
+  }
+`;
+
+export const Content = styled.div`
+  font-size: var(--fontSuperBig);
+  font-weight: 900;
+  text-align: center;
+`;
---
+++ src/components/AddToWatch/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Wrapper, Content } from "./AddToWatch.styles";
+
+const Error = () => (
+  <Wrapper>
+    <Content>It's Lonely Here... 😟</Content>
+    <h3>[Add Some Movies To Your List 😉]</h3>
+  </Wrapper>
+);
+
+export default Error;
