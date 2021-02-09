ad51dedc39e2e1c842f85b59c37fda1097fae387
neeraj1bh <neeraj1bh@gmail.com>
2021-02-09 13:08:34 +0530

    Added toggle logic

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import API from "../API";
 import { isPersistedState } from "../helpers";
+import { useAuth } from "../contexts/GlobalContext";
+
 const initialState = {
   page: 0,
   results: [],
@@ -14,17 +16,50 @@ export const useHomeFetch = () => {
   const [error, setError] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const { toggle } = useAuth();
 
   const fetchMovies = async (page, searchTerm = "") => {
     try {
       setError(false);
       setLoading(true);
-      const movies = await API.fetchMovies(searchTerm, page);
-      setState((prev) => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
-      }));
+      // HomePage result
+      const allResult = await API.fetchMovies(searchTerm, page);
+      console.log(allResult);
+      // SearchPage no duplicates result
+      const ndResults = allResult.results.filter(
+        (movie) => movie.vote_count > "10"
+      );
+      const movies = { ...allResult, results: ndResults };
+      // Exact result
+      const eResults = allResult.results.filter(
+        (movie) => movie.title.toLowerCase() === searchTerm.toLowerCase()
+      );
+      const toggledMovies = { ...allResult, results: eResults };
+      console.log(movies);
+      console.log(searchTerm);
+      searchTerm === null
+        ? setState((prev) => ({
+            ...allResult,
+            results:
+              page > 1
+                ? [...prev.results, ...allResult.results]
+                : [...allResult.results],
+          }))
+        : toggle
+        ? setState((prev) => ({
+            ...toggledMovies,
+            results:
+              page > 1
+                ? [...prev.results, ...toggledMovies.results]
+                : [...toggledMovies.results],
+          }))
+        : setState((prev) => ({
+            ...movies,
+            results:
+              page > 1
+                ? [...prev.results, ...movies.results]
+                : [...movies.results],
+          }));
     } catch (e) {
       setError(true);
     }
